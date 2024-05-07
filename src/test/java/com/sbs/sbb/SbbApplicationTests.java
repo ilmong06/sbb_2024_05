d90596180c7663f17b64e2e136b9180c59044aa3
ilmong06 <ilmongcoding@gmail.com>
2024-05-07 16:21:49 +0900

    데이터 수정

--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -42,8 +43,14 @@ class SbbApplicationTests {
             assertEquals("sbb가 무엇인가요?", q.getSubject());
         }*/
 
-		Question q = this.questionRepository.findBySubjectAndContent(
+		/*Question q = this.questionRepository.findBySubjectAndContent(
 				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1, q.getId());
+		assertEquals(1, q.getId());*/
+
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
 	}
 }
