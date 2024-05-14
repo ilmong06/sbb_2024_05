4c9cf42a5a139cc6d41729c0f7fa089648c94bc2
ilmong06 <ilmongcoding@gmail.com>
2024-05-14 18:23:51 +0900

    12

--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -1,33 +1,31 @@
 package com.sbs.sbb;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest(classes = SbbApplication.class)
+import java.time.LocalDateTime;
+
+
+@SpringBootTest
 class SbbApplicationTests {
 
 	@Autowired
-	private MockMvc mockMvc;
+	private QuestionRepository questionRepository;
 
 	@Test
-	void testWritePage() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/write")) // "/write" 경로에 GET 요청을 보냄
-				.andExpect(MockMvcResultMatchers.status().isOk()) // HTTP 상태코드가 200인지 확인
-				.andExpect(MockMvcResultMatchers.content().string("<!DOCTYPE html>\n" +
-						"<html lang=\"ko\">\n" +
-						"<head>\n" +
-						"    <meta charset=\"UTF-8\">\n" +
-						"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-						"    <title>안녕하세요</title>\n" +
-						"</head>\n" +
-						"<body>\n" +
-						"<h1>안녕하세요</h1>\n" +
-						"</body>\n" +
-						"</html>")); // 응답 본문이 주어진 HTML 코드와 일치하는지 확인
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);  // 첫번째 질문 저장
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);  // 두번째 질문 저장
 	}
 }
