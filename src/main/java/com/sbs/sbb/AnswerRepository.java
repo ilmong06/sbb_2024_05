109c353434625d8bff3344fa90bce6016d6eeffc
ilmong06 <ilmongcoding@gmail.com>
2024-05-14 16:23:02 +0900

    5강 시큐리티

--- src/main/java/com/sbs/sbb/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sbs.sbb;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-
-}
--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -1,69 +1,33 @@
 package com.sbs.sbb;
 
-import org.antlr.v4.runtime.misc.LogManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest
+@SpringBootTest(classes = SbbApplication.class)
 class SbbApplicationTests {
 
 	@Autowired
-	private QuestionRepository questionRepository;
-	private AnswerRepository answerRepository;
-
+	private MockMvc mockMvc;
 
 	@Test
-	void testJpa() {
-		/*Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-	    q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
-
-       List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-       assertEquals("sbb가 무엇인가요?", q.getSubject());
-
-       Optional<Question> oq = this.questionRepository.findById(1);
-       if(oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }*/
-
-		/*Question q = this.questionRepository.findBySubjectAndContent(
-				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1, q.getId());*/
-
-//		assertEquals(2, this.questionRepository.count());
-//		Optional<Question> oq = this.questionRepository.findById(1);
-//		assertTrue(oq.isPresent());
-//		Question q = oq.get();
-//		this.questionRepository.delete(q);
-//		assertEquals(1, this.questionRepository.count());
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
+	void testWritePage() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/write")) // "/write" 경로에 GET 요청을 보냄
+				.andExpect(MockMvcResultMatchers.status().isOk()) // HTTP 상태코드가 200인지 확인
+				.andExpect(MockMvcResultMatchers.content().string("<!DOCTYPE html>\n" +
+						"<html lang=\"ko\">\n" +
+						"<head>\n" +
+						"    <meta charset=\"UTF-8\">\n" +
+						"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+						"    <title>안녕하세요</title>\n" +
+						"</head>\n" +
+						"<body>\n" +
+						"<h1>안녕하세요</h1>\n" +
+						"</body>\n" +
+						"</html>")); // 응답 본문이 주어진 HTML 코드와 일치하는지 확인
 	}
 }
